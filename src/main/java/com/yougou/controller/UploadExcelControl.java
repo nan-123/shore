bea1176e63e8c56a10893abf00b3171d5fb5b714
nan <280173077@qq.com>
2019-05-15 18:35:16 +0800

    用户下载

--- src/main/java/com/yougou/controller/UploadExcelControl.java
+++ src/main/java/com/yougou/controller/UploadExcelControl.java
@@ -1,7 +1,11 @@
 package com.yougou.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.yougou.pojo.User;
 import com.yougou.service.IUploadExcelService;
+import com.yougou.service.IUserService;
+import com.yougou.util.ExcelUtil;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URL;
@@ -22,6 +27,9 @@ public class UploadExcelControl {
 
 	 @Autowired
      private IUploadExcelService service;
+
+	 @Autowired
+     private IUserService userService;
     /** 
      * 描述：通过 jquery.form.js 插件提供的ajax方式上传文件 
      * @throws Exception
@@ -79,6 +87,70 @@ public class UploadExcelControl {
                 bis.close();
             if (bos != null)
                 bos.close();
+        }
+    }
+
+    /**
+     * 导出报表
+     * @return
+     */
+    @RequestMapping(value = "/export")
+    @ResponseBody
+    public void export(HttpServletResponse response){
+        //获取数据
+        List<User> list = userService.getUser(new User());
+
+        //excel标题
+        String[] title = {"姓名","年龄","登录名","部门","性别","邮箱"};
+
+        String [][] content = new String[list.size()][];
+
+         //excel文件名
+         String fileName = "用户信息表"+System.currentTimeMillis()+".xls";
+
+     //sheet名
+        String sheetName = "用户信息表";
+
+            for (int i = 0; i < list.size(); i++) {
+            content[i] = new String[title.length];
+            User user = list.get(i);
+            content[i][0] = user.getName();
+            content[i][1] = user.getAge();
+            content[i][2] = user.getLoginName();
+            content[i][3] = user.getDept();
+            content[i][4] = user.getSex();
+            content[i][5] = user.getMail();
+    }
+
+      //创建HSSFWorkbook
+              HSSFWorkbook wb = ExcelUtil.getHSSFWorkbook(sheetName, title, content, null);
+
+      //响应到客户端
+      try {
+        this.setResponseHeader(response, fileName);
+           OutputStream os = response.getOutputStream();
+           wb.write(os);
+           os.flush();
+           os.close();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+  }
+
+    //发送响应流方法
+    public void setResponseHeader(HttpServletResponse response, String fileName) {
+        try {
+            try {
+                fileName = new String(fileName.getBytes(),"ISO8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            response.setContentType("application/octet-stream;charset=ISO8859-1");
+            response.setHeader("Content-Disposition", "attachment;filename="+ fileName);
+            response.addHeader("Pargam", "no-cache");
+            response.addHeader("Cache-Control", "no-cache");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
