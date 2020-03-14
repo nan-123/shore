52227112da48b80eaf6433eb5da350a2f4d9b001
280173077@qq.com <nan8326113>
2020-03-14 16:38:17 +0800

    Merge remote-tracking branch 'remotes/origin/master' into dev-ces
    # Conflicts:
    #	src/main/java/com/yougou/util/ExcelUtil.java

--- src/main/java/com/yougou/util/ExcelUtil.java
+++ src/main/java/com/yougou/util/ExcelUtil.java
@@ -27,9 +27,7 @@ public class ExcelUtil {
         // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
         HSSFRow row = sheet.createRow(0);
 
-        // 第四步，创建单元格，并设置值表头 设置表头居中
-        // 第四步，创建单元格，并设置值表头 设置表头居中
-        // 第四步，创建单元格，并设置值表头 设置表头居中
+       
         HSSFCellStyle style = wb.createCellStyle();
         style.setAlignment(HSSFCellStyle.ALIGN_CENTER); // 创建一个居中格式
 
