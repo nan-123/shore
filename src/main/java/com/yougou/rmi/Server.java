4b87e51d1198fc381f022e8ea00469bce914d1ef
280173077@qq.com <nan8326113>
2020-05-17 08:52:54 +0800

    Merge remote-tracking branch 'remotes/origin/dev-ces'
    # Conflicts:
    #	src/main/java/com/yougou/util/ExcelUtil.java

--- src/main/java/com/yougou/rmi/Server.java
+++ src/main/java/com/yougou/rmi/Server.java
@@ -8,9 +8,9 @@ public class Server {
 
     public static void main(String[] args) {
         try {
-            HelloService helloService = new HelloService();
-            LocateRegistry.createRegistry(1099);
-            Naming.rebind("rmi://127.0.0.1/hello", helloService);
+            HelloService helloService = new HelloService(); // 已经发布了远程对象helloservice_stub
+            LocateRegistry.createRegistry(1099); // 发布Resgister_stub
+            Naming.rebind("rmi://127.0.0.1/hello", helloService); // 绑定，跟注册中心类似
             System.out.println("服务启动成功");
         } catch (Exception e) {
             e.printStackTrace();
